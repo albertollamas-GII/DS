ace2f47bd36bf88df06616b152e7f511bfe491dc
Astropollo <luisguebat@hotmail.com>
2022-03-07 22:24:44 +0100

    Version 1.2

--- Practica1-Sesion2/src/main/java/Paquete/Practica1Sesion2.java
+++ Practica1-Sesion2/src/main/java/Paquete/Practica1Sesion2.java
@@ -5,6 +5,8 @@
 
 package Paquete;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author luisg
@@ -12,6 +14,7 @@ package Paquete;
 public class Practica1Sesion2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame Pantalla = new JFrame();
+        
     }
 }
