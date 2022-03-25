3830ad430af66b01093f48030267fe532ec7725e
albertollamass <alberto.ll.go@gmail.com>
2022-03-25 10:55:59 +0100

    S3 proyecto netbeans

--- P1/S3/src/s3/ClienteMotor.java
+++ P1/S3/src/s3/ClienteMotor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Paquete;
+package s3;
 
 import java.util.Observable;
 import java.util.logging.Level;
