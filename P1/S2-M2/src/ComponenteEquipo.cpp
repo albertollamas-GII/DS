138a118497a5e7ca7f072e4e743ac395b43f952f
Astropollo <luisguebat@hotmail.com>
2022-03-25 12:04:03 +0100

    ejecutar

--- P1/S2-M2/src/ComponenteEquipo.cpp
+++ P1/S2-M2/src/ComponenteEquipo.cpp
@@ -3,10 +3,6 @@
 #include "ComponenteEquipo.h"
 #include "VisitanteEquipo.h"
 
-ComponenteEquipo::ComponenteEquipo(float precio, std::string nombre){
-    this->precio = precio;
-    this->nombre = nombre;
-}
 
 std::string ComponenteEquipo::getNombre()const{
     return nombre;
